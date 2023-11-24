c1f2cb703f64c59970bbd22b526d74781f1da991
Milana <Milana@MacBook-Air.local>
2023-11-24 05:03:37 +0300

    add changes for llm for one file and added keyword pipeline and examples

--- asr_transcribe_text.py
+++ asr_transcribe_text.py
@@ -82,21 +82,3 @@ if __name__ == "__main__":
         model,
         parsed_args.model_type,
     )
-
-
-for audio_name in os.listdir("train_data/audiofiles/"):
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model="openai/whisper-large-v3",
-        chunk_length_s=30,
-        device=DEVICE,
-        return_timestamps=True,
-    )
-    waveform, sr = torchaudio.load(f"train_data/audiofiles/{audio_name}")
-    waveform = torchaudio.functional.resample(waveform, orig_freq=sr, new_freq=16000)
-    waveform = np.mean(waveform.numpy(), axis=0)
-
-    prediction = pipe(waveform, batch_size=6)
-    with open(f'{str.split(audio_name, ".")[0]}.json', "w", encoding="utf-8") as jsf:
-        json.dump(prediction, jsf, ensure_ascii=False, indent=4)
-    print(prediction)
--- llm_test.py
+++ llm_test.py
@@ -4,33 +4,21 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import json
 
-DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-model = AutoModelForCausalLM.from_pretrained('Open-Orca/Mistral-7B-OpenOrca', device_map='cuda:0', torch_dtype=torch.float16, use_flash_attention_2=True)
-tokenizer = AutoTokenizer.from_pretrained('Open-Orca/Mistral-7B-OpenOrca')
-
-#print(tokenizer.encode('.'))
-#print(tokenizer.encode('\n'))
-
-with torch.inference_mode():
-    for name in os.listdir('train_data/audiofiles/'):
-        with open(str.replace(name, ".mp3", ".json"), 'r', encoding='utf-8') as jsf:
-            text = json.load(jsf)
-
+def get_key_llm(text, model, tokenizer, device="cuda:0"):
+    with torch.inference_mode():
         answer_list = []
         for part in range(0, len(text["text"]) // 6000 + 1):
             user_text = f'I have the following document:\n\n{text["text"][part*6000:(part+1)*6000]}\n\nBased on the information above, extract the keywords that best describe the topic of the text.\nMake sure to only extract keywords that appear in the text.\nUse the following format separated by commas:\n<keywords>\nAnswer in Russian.'
-            #user_text = f'I have the following document:\n\n{text["text"][part * 6000:(part + 1) * 6000]}\n\nBased on the information above, extract the keywords that best describe the topic of the text.\nMake sure to only extract keywords that appear in the text.\nInclude only the most important keywords.\nUse the following format separated by commas:\n<keywords>\nAnswer in Russian.'
-            #user_text = f'I have the following document:\n\n{text["text"][part * 6000:(part + 1) * 6000]}\n\nBased on the information above, extract the keywords that best describe the topic of the text.\nMake sure to only extract keywords that appear in the text.\nGive as few keywords as possible.\nUse the following format separated by commas:\n<keywords>\nAnswer in Russian.'
             chat = [
                 {"role": "system", "content": "The following is a conversation with an AI Large Language Model. The AI has been trained to answer questions, provide recommendations, and help with decision making. The AI follows user requests. The AI thinks outside the box."},
                 {"role": "user", "content": user_text},
             ]
 
             input_tokens = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=False)
-            print(f'len chat tokens (chat tokens + answer must be below 4096): {len(input_tokens)}')
+            print(f'Length of chat tokens (chat tokens + answer must be below 4096): {len(input_tokens)}')
             input_len = len(input_tokens)
-            input_tokens = torch.LongTensor([input_tokens]).to(device=DEVICE)
+            input_tokens = torch.LongTensor([input_tokens]).to(device=device)
 
             out = model.generate(
                 input_ids=input_tokens,
@@ -39,11 +27,26 @@ with torch.inference_mode():
                 repetition_penalty=1.2,
                 #eos_token_id=[32000, 28705],
             )
-            print(out.shape)
             answer = tokenizer.batch_decode(out[:,input_len:], skip_special_tokens=True)[0]
             answer_list.append(str.strip(answer))
-            print(answer)
 
         with open(f'{str.replace(name, ".mp3", "_keywords.json")}', 'w', encoding='utf-8') as jsf:
             json.dump(answer_list, jsf, ensure_ascii=False, indent=4)
+        
+    return answer_list
+
+if __name__ == "__main__":
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+    model = AutoModelForCausalLM.from_pretrained('Open-Orca/Mistral-7B-OpenOrca', device_map=device,
+                                                torch_dtype=torch.float16, use_flash_attention_2=True)
+    tokenizer = AutoTokenizer.from_pretrained('Open-Orca/Mistral-7B-OpenOrca')
+
+    for name in os.listdir('./train_data/audiofiles/'):
+        with open(str.replace(name, ".mp3", ".json"), 'r', encoding='utf-8') as jsf:
+            text = json.load(jsf)
+
+            get_key_llm(text, model, tokenizer, device)
+
+
 
